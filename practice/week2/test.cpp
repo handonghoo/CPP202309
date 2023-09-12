abbd6ab5e77dbd62b057b352fd0bc7e0160ee01a
handonghoo <handonghoo0812@gmail.com>
2023-09-12 16:53:57 +0900

    commit for test

---
+++ practice/week2/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 15;
+	cout << a + b << endl;
+
+	char text1[10] = "Hello";
+	char text2[50] = "World";
+
+	cout << text1 <<  "!"  << endl;
+	cout << text2 <<  "!" << endl;
+
+	return 0;
+}
